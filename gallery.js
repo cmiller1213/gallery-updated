6fe06e962c38761e8e8c4b8483edbbd7bb091bcd
Catherine <catherinemllr13@gmail.com>
2018-09-27 18:56:32 -0400

    Class Gallery
    Initial class gallery

---
+++ gallery.js
@@ -0,0 +1,16 @@
+//wait until the HTML document is ready
+
+	//select all the slides (array)
+	var slides = $('.slide');
+	//set the index slide (number)
+	var slideIndex = 0;
+	//what's the current slide?(indexing into an array)
+	var currentSlide = slides[slideIndex];
+
+	//hide all slides
+
+	//show the first one
+
+	//when a user clicks next (function)
+
+	//when a user clicks previous (function)
