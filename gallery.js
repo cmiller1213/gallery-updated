69f97c8fab94d24e3d93a37723d4587a66fb542c
Catherine <catherinemllr13@gmail.com>
2018-10-01 15:57:39 -0400

    Add Javascript
    included class code

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 //wait until the HTML document is ready
-
+$(document).ready(function(){
 	//select all the slides (array)
 	var slides = $('.slide');
 	//set the index slide (number)
@@ -8,9 +8,27 @@
 	var currentSlide = slides[slideIndex];
 
 	//hide all slides
-
+	slides.hide();
 	//show the first one
-
+	slides.first().show();
 	//when a user clicks next (function)
-
+	$('.next').click(function(){
+		$(currentSlide).hide();
+		slideIndex++;
+		if (slideIndex > 4){
+			slideIndex = 0;
+		}
+		currentSlide = slides[slideIndex];
+		$(currentSlide).show();
+	});
 	//when a user clicks previous (function)
+	$('.previous').click(function(){
+		$(currentSlide).hide();
+		slideIndex--;
+		if(slideIndex < 0){
+			slideIndex = 4;
+		}
+		currentSlide = slides[slideIndex];
+		$(currentSlide).show();
+	});
+});
