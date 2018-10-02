bd8ea43b71d39a125327854eb1cf08886ed94687
Catherine <catherinemllr13@gmail.com>
2018-10-02 13:47:58 -0400

    No changes

--- gallery.js
+++ gallery.js
@@ -1,60 +1,34 @@
+//wait until the HTML document is ready
 $(document).ready(function(){
-
-	// select all the slides (array)
-	var slides = $(".slide");
-	// set a index slide (number); where we start
+	//select all the slides (array)
+	var slides = $('.slide');
+	//set the index slide (number)
 	var slideIndex = 0;
-	// what's the current slide? (indexing into an array)
+	//what's the current slide?(indexing into an array)
 	var currentSlide = slides[slideIndex];
 
-	// hide all slides with the hide() method
+	//hide all slides
 	slides.hide();
-	// this is the equivalent to saying 
-	// $(".slide").hide();
-	// console.log("There are this many slides: " + slides.length);
-
-	// show the first one
+	//show the first one
 	slides.first().show();
-
-	// when a user clicks "next"... (function)
-	$(".next").click(function(){
-		// hide current slide
+	//when a user clicks next (function)
+	$('.next').click(function(){
 		$(currentSlide).hide();
-
-		// increment the slideIndex (increases the slide index)
-		slideIndex++; // slideIndex = slideIndex + 1
-
-		// if the slideIndex is greater than 4... (conditional)
+		slideIndex++;
 		if (slideIndex > 4){
-			// reset the slideIndex to 0
 			slideIndex = 0;
 		}
-
-		// set the currentSlide as the new slideIndex (indexing into the array) among the set of slides
 		currentSlide = slides[slideIndex];
-
-		// show the currentSlide
 		$(currentSlide).show();
-	}); // close the next function
-	// when a user clicks "previous" (function)
-	$(".previous").click(function(){
-		// hide the current slide
+	});
+	//when a user clicks previous (function)
+	$('.previous').click(function(){
 		$(currentSlide).hide();
-
-		// decrement the slideIndex
-		slideIndex--; // slideIndex = slideIndex - 1
-
-		// if the slideIndex is less than 0... (condition)
-		if (slideIndex < 0){
-			// reset the slideIndex to 4
+		slideIndex--;
+		if(slideIndex < 0){
 			slideIndex = 4;
 		}
-
-		// set the new slideIndex as the currentSlide
 		currentSlide = slides[slideIndex];
-		// show the currentSlide
 		$(currentSlide).show();
-
-	}); // close the previous function
-}); // close document ready function
-
+	});
+});
